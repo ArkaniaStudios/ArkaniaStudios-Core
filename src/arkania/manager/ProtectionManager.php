cc7cb9d4343a86192ef5902ece3bcd62319481f5
Dumont-Julien <61889427+Dumont-Julien@users.noreply.github.com>
2023-03-05 13:18:26 +0100

    Release 1.0.0

--- src/arkania/manager/ProtectionManager.php
+++ src/arkania/manager/ProtectionManager.php
@@ -17,39 +17,6 @@ declare(strict_types=1);
 
 namespace arkania\manager;
 
-use pocketmine\player\Player;
-
 final class ProtectionManager {
 
-    /**
-     * @param Player $player
-     * @param string $zoneType
-     * @return bool
-     */
-    public static function canModifyZone(Player $player, string $zoneType = 'spawn'): bool {
-        $spawn = [
-            'minX' => 100,
-            'minZ' => -100,
-            'maxX' => -100,
-            'maxZ' => 100
-        ];
-        $warzone = [
-            'minX' => 200,
-            'minZ' => -200,
-            'maxX' => -200,
-            'maxZ' => 200
-        ];
-        if (strtolower($zoneType) === 'spawn')
-            if ($player->getPosition()->getX() >= $spawn['minX'] || $player->getPosition()->getX() <= $spawn['maxX'] || $player->getPosition()->getZ() >= $spawn['minZ'] || $player->getPosition()->getZ() <= $spawn['maxZ'])
-                return true;
-            else
-                return false;
-        elseif(strtolower($zoneType) === 'warzone')
-            if ($player->getPosition()->getX() >= $warzone['minX'] || $player->getPosition()->getX() <= $warzone['maxX'] || $player->getPosition()->getZ() >= $warzone['minZ'] || $player->getPosition()->getZ() <= $warzone['maxZ'])
-                return true;
-            else
-                return false;
-        else
-            return true;
-    }
 }
